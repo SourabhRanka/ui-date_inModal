7137dc1c1d93b26aa02ee36657cd84eed18af0ce
sranka2 <sranka2@csc.com>
2017-09-03 15:48:23 +0530

    creating app

---
+++ app.js
@@ -0,0 +1,14 @@
+var app = angular.module('plunker', ['ui.directives']);
+
+app.controller('MainCtrl', function($scope) {
+  
+  $scope.opts = {
+    dateFormat: 'dd/mm/yy',
+    changeMonth: true,
+    changeYear: true,
+    container:'.modal-body'
+  };
+  $scope.data = {
+    valor: "10/09/2013"
+  };
+});
